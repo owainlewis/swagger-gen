1f8afca794c8b4dda5fd158415014dfbb48c6cf8
Owain Lewis <owain@owainlewis.com>
2015-09-06 21:23:42 +0100

    updates

--- src/swagger_gen/core.clj
+++ src/swagger_gen/core.clj
@@ -3,11 +3,18 @@
             [cheshire.core :as json]
             [swagger-gen.util :refer [normalize-def]]
             [yaml.core :as yml]))
-          
-(defn load-swagger 
-  "Load a swagger spec from disk. TODO make polymorphic over formats"
+
+(defn yaml-path? [path-to-file]
+  (or (.endsWith path-to-file ".yaml")
+      (.endsWith path-to-file ".yml")))
+
+(defn load-swagger
+  "Load a swagger spec from disk.
+   File can be either yaml or json format"
   [path-to-file]
-  (yml/from-file path-to-file false))
+  (if (yaml-path? path-to-file)
+    (yml/from-file path-to-file false)
+    (->> path-to-file slurp json/parse-string)))
 
 (defn- get-section
   "Util function for extracting swagger spec sections
@@ -30,19 +37,25 @@
                 :type (normalize-def (or (:type attrs) (:$ref attrs)))
                 :items (or (vals (:items attrs)) [])
                 :required (contains? required-attributes property)}))}))
-  
+
 (defn get-definitions [spec]
   (when-let [data (keywordize-keys (get-section spec :definitions))]
     (map (fn [definition]
            (let [[class-name attributes] definition]
              {:name (name class-name)
               :attributes attributes})) data)))
-    
+
 (defn swagger-defs
   "Extract all swagger definitions/models from a spec"
   [spec]
   (when-let [definitions (get-definitions spec)]
     (map normalize-definition definitions)))
+
+(defn params-of-type [swagger-route param-type]
+  (->> swagger-route :parameters (filter #(= (:in %) param-type)) (into [])))
+
+(defn query-params [swagger-route]
+  (params-of-type swagger-route "query"))
 
 (defn swagger-paths
   "Extract all HTTP request paths from a swagger spec"
@@ -55,10 +68,14 @@
             (merge {:method method :path k}
                    (keywordize-keys attributes))))))))
 
+(defn swagger-info [spec]
+  (get-section spec :info))
+
 (defn parse-swagger
-  "Load a swagger specification from file path and convert it into 
+  "Load a swagger specification from file path and convert it into
    a sane/traversable format making it easier to work with"
   [path-to-swagger]
   (let [data (load-swagger path-to-swagger)]
-    {:paths (swagger-paths data)
+    {:info (swagger-info data)
+     :paths (swagger-paths data)
      :definitions (swagger-defs data)}))
--- src/swagger_gen/main.clj
+++ src/swagger_gen/main.clj
@@ -5,16 +5,22 @@
 
 (def spec "resources/swagger/petstore.yaml")
 
+;; TODO use functions here from clostache
+
 (defn generate-scala-models [spec]
   (let [swagger-definitions (:definitions (core/parse-swagger spec))]
     (map #(assoc % :repr (scala/render-case-class %)
                    :count (count (:args %)))
          swagger-definitions)))
 
+(defn generate-scala-routes [spec]
+  (render-resource "templates/scala/routes.mustache"
+    (core/parse-swagger spec)))
+
 (defn code-generate [spec]
   (let [models (generate-scala-models spec)]
     (render-resource "templates/scala/models.mustache"
-      {:models models})))
+      (core/parse-swagger spec))))
 
 (defn -main []
   (println "Generating code...")
