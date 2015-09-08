f39b2b8df593f3dfb35607a9a39e3c8c10538f8f
Owain Lewis <owain@owainlewis.com>
2015-09-08 18:06:08 +0100

    Update core.clj

--- src/swagger_gen/core.clj
+++ src/swagger_gen/core.clj
@@ -53,16 +53,21 @@
     (map normalize-definition definitions)))
 
 (defn params-of-type 
+  "Extract swagger params of a given type i.e :body or :path"
   [swagger-route param-type]
   (->> swagger-route 
        :parameters 
        (filter #(= (:in %) param-type))
        (into [])))
 
-(defn body-params [swagger-route]
+(defn body-params 
+  "Extract one or more body params from a sswagger path"
+  [swagger-route]
   (params-of-type swagger-route "body"))
 
-(defn query-params [swagger-route]
+(defn query-params
+  "Extract one or more query params from a swagger path"
+  [swagger-route]
   (params-of-type swagger-route "query"))
 
 (defn swagger-paths
