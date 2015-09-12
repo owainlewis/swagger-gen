1486c3c4be27864c33421b04504c2590681c9926
Owain Lewis <owain@owainlewis.com>
2015-09-12 10:25:01 +0100

    adding golang example

---
+++ src/swagger_gen/examples/golang/generator.clj
@@ -0,0 +1,28 @@
+(ns swagger-gen.examples.golang.generator
+  (:require [swagger-gen.core :refer [render-swagger]]))
+
+(defn extract-route-param-names [normalized-path]
+  (->> normalized-path
+       :parameters
+       (filter #(= (:in %) "path"))
+       (map :name)))
+
+(defn path-to-go-route [normalized-path]
+  (let [extractor (juxt :path :operationId :method :summary)
+        [path operation method doc] (extractor normalized-path)]
+  {:path      path
+   :method    (.toUpperCase method)
+   :operation operation
+   :params    (extract-route-param-names normalized-path)
+   :doc       doc }))
+
+(def spec "resources/swagger/petstore.yaml")
+
+(defn generate []
+  (let [template "src/swagger_gen/examples/golang/template.mustache"]
+    (render-swagger spec template
+      (fn [spec]
+        {:routes (map path-to-go-route (:normalized-paths spec))}))))
+
+(defn -main []
+  (spit "src/swagger_gen/examples/golang/main.go" (generate)))
