bd537f9deee4534b0b64c6379e1ca50d5724f53d
Owain Lewis <owain@owainlewis.com>
2015-09-15 10:09:45 +0100

    Update core.clj

--- src/swagger_gen/core.clj
+++ src/swagger_gen/core.clj
@@ -93,14 +93,14 @@
   "Render a swagger spec to a given template. 
    
    We can optionally pass in a transfomer function that takes a swagger spec
-   and enriches or alters it with a custome function. This could be used
+   and enriches or alters it with a custom function. This could be used
    to *merge* additional arguments 
    
    (fn [spec] (merge additional-params spec))
    
    (fn [spec] (assoc spec :foo "bar"))
    
-   or transform a swagger spec by restructuring it
+   or *transform* a swagger spec by restructuring it
    in a way that is more acceptable for template rendering.
    
    (fn [spec] (transform spec))
