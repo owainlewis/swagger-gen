ee19e1f7fbeb61e4344a71a4b0336733e389ae11
Owain Lewis <owain.lewis@monitise.com>
2015-09-15 12:57:53 +0000

    1.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.forward/swagger-gen "0.1.0-SNAPSHOT"
+(defproject io.forward/swagger-gen "1.0.0-SNAPSHOT"
   :description "Swagger code generation toolkit"
   :url "http://github.com/owainlewis"
   :license {:name "Eclipse Public License"
