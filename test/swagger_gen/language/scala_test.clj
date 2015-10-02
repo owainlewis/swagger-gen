aca8fc323d1fc9048ad18d201645fdf0008e4062
Owain Lewis <owain.lewis@monitise.com>
2015-10-02 15:18:18 +0000

    Adds enumerations for case classes
    Updates to 1.0.6

--- test/swagger_gen/language/scala_test.clj
+++ test/swagger_gen/language/scala_test.clj
@@ -10,13 +10,28 @@
     (let [route {:name "Foo" :args []}]
       (is (= "case object Foo" (render-case-class route true))))))
 
+(deftest enum-type-string-test []
+  (testing "should generate enum string"
+    (is (= "FooBarEnum.Type" (enum-type-string :foo :bar)))))
+
+(deftest optional?-test []
+  (testing "should convert to an option type"
+    (is (= "Option[Foo] = None" (optional? false "Foo")))
+    (is (= "Foo"                (optional? true  "Foo")))))
+
+(deftest optional-enum-type-string-test []
+  (testing "should generate optional enum string"
+    (is (= "Option[FooBarEnum.Type] = None"
+           (optional? false (enum-type-string :foo :bar))))))
+
+(deftest enum-case-class-test []
+  (testing "should render Scala enum types"
+    (let [expected "case class Error(code: Int, message: ErrorMessageEnum.Type)"
+          actual (render-case-class error-definition true)]
+      (is (= expected actual)))))
+
 (deftest optional-params-case-class-test []
   (testing "should handle optional params"
     (let [expected "case class Error(code: Option[Int] = None, message: Option[String] = None)"
           actual (render-case-class error-definition-with-optional-params true)]
       (is (= expected actual)))))
-
-(deftest generate-case-class-test []
-  (testing "should generate a Scala case class"
-    (is (= "case class Error(code: Int, message: String)"
-           (render-case-class error-definition true)))))
