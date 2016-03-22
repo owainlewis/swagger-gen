426a62134259d59e6b61e3c221c7fe31b15a2abb
Owain Lewis <owain@owainlewis.com>
2016-03-22 06:16:50 +0000

    Fix tests

--- test/swagger_gen/core_test.clj
+++ test/swagger_gen/core_test.clj
@@ -16,13 +16,13 @@
 (deftest parse-swagger-test []
   (testing "it should parse a swagger spec"
     (is (= (keys petstore)
-           '(:schemes :definitions :produces :paths :consumes :host :info :swagger :basePath)))))
+           '(:swagger :info :host :basePath :schemes :consumes :produces :paths :definitions)))))
 
 (deftest parse-swagger-string-test []
   (testing "it should parse a swagger spec string"
     (let [from-string (parse-swagger-string (slurp petstore-yaml))]
-    (is (= (keys from-string)
-           '(:schemes :definitions :produces :paths :consumes :host :info :swagger :basePath))))))
+      (is (= (keys from-string)
+           '(:swagger :info :host :basePath :schemes :consumes :produces :paths :definitions))))))  
 
 (deftest check-paths-test []
   (testing "paths should be a vector of paths containing method and path"
