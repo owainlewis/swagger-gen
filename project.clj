552fc1f63a57092d828335c5e8303d92611afd10
Owain Lewis <owain@owainlewis.com>
2016-04-04 18:50:28 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/owainlewis"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [stencil "0.5.0"]
                  [io.forward/yaml "1.0.3"]
                  [org.flatland/ordered "1.5.2"]])
